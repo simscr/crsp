16375496313afb1f8d060aab8a29486884247268
simscr <simscr90@gmail.com>
2021-10-13 20:18:23 -0500

    rest of initial files

---
+++ R/cs_import.R
@@ -0,0 +1,23 @@
+#' Import a File and Clean Names
+#'
+#' This function combines the import function from the rio package and the clean_names
+#' function from the janitor package. I got tired of using those two functions in
+#' succession for every file.
+#'
+#' @param file The file to be imported. Can be any file type supported by rio::import.
+#' @return A data frame with clean names
+#' @examples
+#' library(datasets)
+#'
+#' #' create CSV to import
+#' rio::export(USArrests, "USArrests.csv")
+#'
+#' #' import CSV and clean names
+#' cs_import("USArrests.csv")
+
+cs_import <- function(file){
+  df <- rio::import(file) %>%
+    janitor::clean_names()
+
+  return(df)
+}
